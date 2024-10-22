4f0f5007f285487d8b14c542943eb6027a6e56fc
Vitaliy <kosh98vitaliy@outlook.com>
2024-10-22 17:22:20 +0300

    realization of Example class

---
+++ src/main/java/Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    private String name;
+
+    public Example(String name) {
+        this.name = name;
+    }
+
+    public void showHello() {
+        System.out.println("Hello, " + name);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Example example = new Example("Vitaliy");
 
+        example.showHello();
     }
 }
